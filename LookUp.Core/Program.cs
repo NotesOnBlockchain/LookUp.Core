581c1c4165a90818b9afcad5c7900b6613a2d3cd
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-09-24 18:55:13 +0200

    initial commit

---
+++ LookUp.Core/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LookUp.Core
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                using var host = CreateHostBuilder(args).Build();
+                await host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:37127/"));
+    }
+}
---
+++ LookUp.Core/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Startup
+{
+	public Startup()
+	{
+	}
+
+    public void ConfigureServices(IServiceCollection services)
+	{
+        services.AddMemoryCache();
+        services.AddMvc();
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddAuthorization();
+
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+        app.UseSwagger();
+    }
+}
