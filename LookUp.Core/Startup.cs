a91bbcbcd4b1111cc03c430376b94a64a20747b5
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-09-25 17:32:40 +0200

    Load config in Startup

--- LookUp.Core/Startup.cs
+++ LookUp.Core/Startup.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using LookUp.Core.Config;
+using LookUp.Core.Helpers;
+using System;
 
 public class Startup
 {
-	public Startup()
-	{
-	}
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
 
     public void ConfigureServices(IServiceCollection services)
 	{
+        string dataDir = Configuration["datadir"] ?? EnvironmentHelpers.GetDataDir(Path.Combine("LookUp", "Backend"));
+        string configFilePath = Path.Combine(dataDir, "Config.json");
+        Config config = Config.LoadFile(configFilePath);
+
         services.AddMemoryCache();
         services.AddMvc();
         services.AddControllers();
