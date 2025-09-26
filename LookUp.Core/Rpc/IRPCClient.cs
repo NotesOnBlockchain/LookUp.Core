b078f853aa98b09b2f86a836bc0caf2f6e4a0629
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-09-26 17:35:24 +0200

    add interface

---
+++ LookUp.Core/Rpc/IRPCClient.cs
@@ -0,0 +1,34 @@
+﻿using LookUp.Core.Rpc.Models;
+using NBitcoin;
+using NBitcoin.RPC;
+
+namespace LookUp.Core.Rpc
+{
+    public interface IRPCClient
+    {
+        Network Network { get; }
+        RPCCredentialString CredentialString { get; }
+
+        Task<uint256> GetBestBlockHashAsync(CancellationToken cancellationToken = default);
+
+        Task<Block> GetBlockAsync(uint256 blockId, CancellationToken cancellationToken = default);
+
+        Task<Block> GetBlockAsync(uint blockHeight, CancellationToken cancellationToken = default);
+
+        Task<BlockHeader> GetBlockHeaderAsync(uint256 blockHash, CancellationToken cancellationToken = default);
+
+        Task StopAsync(CancellationToken cancellationToken = default);
+
+        Task<BlockchainInfo> GetBlockchainInfoAsync(CancellationToken cancellationToken = default);
+
+        Task<uint256[]> GetRawMempoolAsync(CancellationToken cancellationToken = default);
+
+        Task<MemPoolInfo> GetMempoolInfoAsync(CancellationToken cancel = default);
+
+        Task<GetTxOutResponse?> GetTxOutAsync(uint256 txid, int index, bool includeMempool = true, CancellationToken cancellationToken = default);
+
+        IRPCClient PrepareBatch();
+
+        Task<VerboseBlockInfo> GetVerboseBlockAsync(uint256 blockId, CancellationToken cancellationToken = default);
+    }
+}
