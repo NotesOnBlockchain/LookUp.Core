71884435b5f166b84fcb681b12dfdbce0e3042f8
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-09-26 17:35:18 +0200

    add RPC models

---
+++ LookUp.Core/Rpc/Models/VerboseBlockInfo.cs
@@ -0,0 +1,29 @@
+﻿using NBitcoin;
+
+namespace LookUp.Core.Rpc.Models
+{
+    public class VerboseBlockInfo
+    {
+        public VerboseBlockInfo(uint256 prevBlockHash, ulong confirmations, uint256 hash, DateTimeOffset blockTime, ulong height, IEnumerable<VerboseTransactionInfo> transactions)
+        {
+            PrevBlockHash = prevBlockHash;
+            Confirmations = confirmations;
+            Hash = hash;
+            BlockTime = blockTime;
+            Height = height;
+            Transactions = transactions;
+        }
+
+        public DateTimeOffset BlockTime { get; }
+
+        public uint256 Hash { get; }
+
+        public uint256 PrevBlockHash { get; }
+
+        public ulong Confirmations { get; }
+
+        public ulong Height { get; }
+
+        public IEnumerable<VerboseTransactionInfo> Transactions { get; }
+    }
+}
---
+++ LookUp.Core/Rpc/Models/VerboseInputInfo.cs
@@ -0,0 +1,13 @@
+﻿using NBitcoin;
+
+namespace LookUp.Core.Rpc.Models
+{
+    public abstract record VerboseInputInfo
+    {
+        public record Coinbase(string Message) : VerboseInputInfo;
+
+        public record Full(OutPoint OutPoint, VerboseOutputInfo PrevOut) : VerboseInputInfo;
+
+        public record None(OutPoint Outpoint) : VerboseInputInfo;
+    }
+}
