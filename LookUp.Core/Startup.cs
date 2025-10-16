3475eda6d7b96c38dea9f2ceb2d22fc3aa4db1be
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-16 16:58:07 +0200

    fix dependency injection

--- LookUp.Core/Startup.cs
+++ LookUp.Core/Startup.cs
@@ -34,7 +34,7 @@ public class Startup
 
         string lastScannedBlockHeightFilePath = Path.Combine(dataDir, "LastScannedBlockHeight.txt");
         var scannerService = ScannerService.LoadWithConfig(lastScannedBlockHeightFilePath, myRPCClient);
-        services.AddHostedService<ScannerService>();
+        services.AddHostedService<ScannerService>(services => scannerService);
         
 
         services.AddMemoryCache();
