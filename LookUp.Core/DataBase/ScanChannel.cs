53c8052e9bc5d33394143ce78461e244f4aa40d1
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-17 16:55:24 +0200

    Add MessageModel and ScanChannel

---
+++ LookUp.Core/DataBase/ScanChannel.cs
@@ -0,0 +1,20 @@
+﻿using LookUp.Core.Models;
+using System.Threading.Channels;
+
+namespace LookUp.Core.DataBase
+{
+    public class ScanChannel
+    {
+        public Channel<MessageModel> ChannelToDB { get; }
+
+        public ScanChannel()
+        {
+            ChannelToDB = Channel.CreateUnbounded<MessageModel>(
+                new UnboundedChannelOptions
+                {
+                    SingleReader = false,
+                    SingleWriter = false
+                });
+        }
+    }
+}
---
+++ LookUp.Core/Models/MessageModel.cs
@@ -0,0 +1,6 @@
+﻿using NBitcoin;
+
+namespace LookUp.Core.Models
+{
+    public record MessageModel(uint256 TransactionID, string Message, string Hex, uint256 blockHash, uint blockIndex, DateTimeOffset BlockMinedAt);
+}
