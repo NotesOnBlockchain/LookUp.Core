fb1831fe943e76da04806e199373f80743e6adf2
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-21 17:59:13 +0200

    add MessageDbContext

---
+++ LookUp.Core/DataBase/MessageDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using LookUp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LookUp.Core.DataBase
+{
+    public class MessageDatabaseContext : DbContext
+    {
+        public MessageDatabaseContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<MessageModel> Messages { get; set; }
+    }
+}
