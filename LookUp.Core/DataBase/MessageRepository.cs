4db0e8b4c4d20d0d814b26e311caa2b7ca54b877
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-22 17:45:54 +0200

    add simple MessageRepo

---
+++ LookUp.Core/DataBase/MessageRepository.cs
@@ -0,0 +1,51 @@
+﻿using LookUp.Core.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LookUp.Core.DataBase
+{
+    public class MessageRepository
+    {
+        private readonly MessageDatabaseContext _dBContext;
+
+        private object _dBContextLock = new();
+
+        public MessageRepository(MessageDatabaseContext databaseContext)
+        {
+            _dBContext = databaseContext;
+        }
+
+        public List<MessageModel> GetMessages()
+        {
+            lock (_dBContextLock)
+            {
+                return _dBContext.Messages.ToList();
+            }
+        }
+
+        public void AddMessage(MessageModel message)
+        {
+            lock(_dBContextLock)
+            {
+                _dBContext.Messages.Add(message);
+                _dBContext.SaveChanges();
+            }
+        }
+
+        public void RemoveMessage(MessageModel message) 
+        {
+            lock (_dBContextLock) 
+            {
+                _dBContext.Messages.Remove(message);
+                _dBContext.SaveChanges();
+            }
+        }
+        public void Clear() 
+        {
+            lock (_dBContextLock) 
+            {
+                _dBContext.Database.EnsureDeleted();
+                _dBContext.SaveChanges();
+            }
+        }
+    }
+}
