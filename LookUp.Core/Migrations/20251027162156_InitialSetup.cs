3424c6d4ba798056ea463e4975f1108711d617b5
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-27 17:32:28 +0100

    Add initial migration files

---
+++ LookUp.Core/Migrations/20251027162156_InitialSetup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LookUp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    TransactionID = table.Column<string>(type: "text", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false),
+                    Hex = table.Column<string>(type: "text", nullable: false),
+                    BlockHash = table.Column<string>(type: "text", nullable: false),
+                    BlockIndex = table.Column<long>(type: "bigint", nullable: false),
+                    BlockMinedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
