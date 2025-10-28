71c7422e2c7057e3729fd03698ed067752b96d64
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-28 18:10:12 +0100

    Add new migration to drop BlockIndex column

---
+++ LookUp.Core/Migrations/20251028170317_RemoveBlockIndexColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LookUp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBlockIndexColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlockIndex",
+                table: "Messages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BlockIndex",
+                table: "Messages",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
