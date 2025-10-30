086ce3939bd4715273cff9023f10f2b9e529498e
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-10-30 17:08:24 +0100

    check db connection in StartupTask

--- LookUp.Core/StartupTask.cs
+++ LookUp.Core/StartupTask.cs
@@ -1,21 +1,29 @@
-﻿using LookUp.Core.Rpc;
+﻿using LookUp.Core.DataBase;
+using LookUp.Core.Rpc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
+using System.Security.Authentication;
 
 namespace LookUp.Core
 {
     public class StartupTask
     {
-        public StartupTask(IRPCClient client)
+        public StartupTask(IRPCClient client, IServiceScopeFactory scopeFactory)
         {
             RpcClient = client;
+            ScopeFactory = scopeFactory;
         }
         private IRPCClient RpcClient { get; }
+        public IServiceScopeFactory ScopeFactory { get; }
 
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             // Make sure we are connected to Knots
             await CheckRpcConnectionAsync(cancellationToken);
+
+            // Make sure we are connected to the Database
+            await CheckDatabaseConnectionAsync(cancellationToken);
         }
 
         private async Task CheckRpcConnectionAsync(CancellationToken cancellationToken)
@@ -44,6 +52,28 @@ namespace LookUp.Core
             catch (Exception ex) 
             {
                 Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+        private async Task CheckDatabaseConnectionAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                using var scope = ScopeFactory.CreateScope();
+                MessageDatabaseContext databaseContext = scope.ServiceProvider.GetService<MessageDatabaseContext>() ?? throw new Exception($"Couldn't get {typeof(MessageDatabaseContext)}");
+
+                if (!await databaseContext.Database.CanConnectAsync(cancellationToken))
+                {
+                    throw new AuthenticationException();
+                }
+
+                databaseContext.Database.OpenConnection();
+                databaseContext.Database.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Couldn't connect to database: {ex.ToString()}");
                 throw;
             }
         }
