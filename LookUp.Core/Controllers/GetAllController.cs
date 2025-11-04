18c3a0d29b77c1c1a4432ee709b739535b7ba4e5
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-04 17:54:22 +0100

    Add controller

---
+++ LookUp.Core/Controllers/GetAllController.cs
@@ -0,0 +1,31 @@
+﻿using LookUp.Core.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LookUp.Core.Controllers
+{
+    [Route("/getmessages")]
+    public class GetAllController : Controller
+    {
+        public GetAllController(MessageRepository messageRepository)
+        {
+            MessageRepo = messageRepository;
+        }
+        public MessageRepository MessageRepo { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var messages = MessageRepo.GetMessages();
+
+            StringBuilder stringBuilder = new();
+
+            foreach (var message in messages)
+            {
+                stringBuilder.AppendLine(Serialization.Encode.Message(message).ToString());
+            }
+
+            return Ok($"{stringBuilder.ToString()}");
+        }
+    }
+}
