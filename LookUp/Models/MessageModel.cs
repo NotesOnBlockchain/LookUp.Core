9e37ff1b8a0a5ce83823d76fc6e3f76b65731d13
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-05 17:36:12 +0100

    Add shared library

---
+++ LookUp/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LookUp.Models
+{
+    public class MessageModel(string transactionID, string message, string hex, string blockHash, DateTimeOffset blockMinedAt)
+    {
+        [Key]
+        public Guid ID { get; set; } = new Guid();
+        public string TransactionID { get; set; } = transactionID;
+        public string Message { get; set; } = message;
+        public string Hex { get; set; } = hex;
+        public string BlockHash { get; set; } = blockHash;
+        public DateTimeOffset BlockMinedAt { get; set; } = blockMinedAt;
+    }
+}
