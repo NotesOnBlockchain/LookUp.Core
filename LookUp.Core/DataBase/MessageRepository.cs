ae4944e113ce3853f4e530682c2db6c28ceabe9f
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-08 23:29:13 +0100

    Add search method in messageRepo

--- LookUp.Core/DataBase/MessageRepository.cs
+++ LookUp.Core/DataBase/MessageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using LookUp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LookUp.Scanner.DataBase
 {
@@ -28,6 +29,28 @@ namespace LookUp.Scanner.DataBase
                 _dBContext.Messages.Add(message);
                 _dBContext.SaveChanges();
             }
+        }
+
+        public async Task<List<MessageModel>> FindAsync(string query)
+        {
+            DateTimeOffset? dayStart = null;
+            DateTimeOffset? dayEnd = null;
+
+            if (DateTime.TryParse(query, out var parsed))
+            {
+                dayStart = new DateTimeOffset(parsed.Date, TimeSpan.Zero);
+                dayEnd = dayStart.Value.AddDays(1);
+            }
+
+            return await _dBContext.Messages.Where(model =>
+                model.TransactionID.Equals(query) ||
+                EF.Functions.ILike(model.Message, $"%{query}%") ||
+                model.Hex.Equals(query) ||
+                model.BlockHash.Equals(query) ||
+                (dayStart != null && model.BlockMinedAt >= dayStart && model.BlockMinedAt < dayEnd))
+                .Distinct()
+                .ToListAsync();
+            
         }
 
         public void RemoveMessage(MessageModel message) 
