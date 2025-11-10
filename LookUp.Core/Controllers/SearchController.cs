b6097d413ce30e64d5f35b4d98af41aa5053871a
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-10 16:57:38 +0100

    Use Memory Cache

--- LookUp.Core/Controllers/SearchController.cs
+++ LookUp.Core/Controllers/SearchController.cs
@@ -1,26 +1,37 @@
-﻿using LookUp.Scanner.DataBase;
+﻿using LookUp.Models;
+using LookUp.Scanner.DataBase;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace LookUp.Scanner.Controllers
 {
     public class SearchController : Controller
     {
-        public SearchController(MessageRepository messageRepository)
+        public SearchController(MessageRepository messageRepository, IMemoryCache cache)
         {
             MessageRepository = messageRepository;
+            Cache = cache;
         }
 
         private MessageRepository MessageRepository { get; }
+        private IMemoryCache Cache { get; }
 
         [HttpGet("/search")]
         public async Task<IActionResult> SearchAsync([FromQuery] string query)
         {
-           var searchResult = await MessageRepository.FindAsync(query);
+            var cacheKey = $"{query.Trim().ToLower()}";
 
-            if (searchResult.Count == 0 || searchResult is null) 
+            if (Cache.TryGetValue(cacheKey, out List<MessageModel>? cachedResult)) 
             {
-                Ok("No message found");
+                return Ok(cachedResult);
             }
+
+            var searchResult = await MessageRepository.FindAsync(query);
+
+            Cache.Set(cacheKey, searchResult, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60)
+            });
 
             return Ok(searchResult);
         }
