5520dcf61a6164969af12e5f0043686e99f39bf8
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-21 15:23:49 +0100

    test controller

--- LookUp.Core/Controllers/TestController.cs
+++ LookUp.Core/Controllers/TestController.cs
@@ -1,26 +1,37 @@
 ﻿using LookUp.Core.Rpc;
-using LookUp.Core.Serialization;
+using LookUp.Scanner.DataBase;
+using LookUp.Serialization;
 using Microsoft.AspNetCore.Mvc;
-using NBitcoin;
+using System.Text;
 
-namespace LookUp.Core.Controllers
+namespace LookUp.Scanner.Controllers
 {
     [Route("/test")]
     public class TestController : Controller
     {
-        public TestController(IRPCClient rpcClient)
+        public TestController(IRPCClient rpcClient, MessageRepository messageRepository)
         {
             RpcClient = rpcClient;
+            MessageRepo = messageRepository;
         }
 
         public IRPCClient RpcClient { get; }
+        public MessageRepository MessageRepo { get; }
 
         [HttpGet]
         public async Task<IActionResult> TestAsync()
         {
-            uint256 bestBlockHash = await RpcClient.GetBestBlockHashAsync().ConfigureAwait(false);
+            var messages = MessageRepo.GetMessages();
+            Console.WriteLine($"Messages Count: {messages.Count}");
 
-            return Ok(JsonEncoder.ToString(bestBlockHash, Encode.UInt256));
+            StringBuilder stringBuilder = new();
+
+            foreach (var message in messages) 
+            {
+                stringBuilder.AppendLine(Encode.Message(message).ToString());
+            }
+
+            return Ok($"{stringBuilder.ToString()}");
         }
     }
 }
