42b76fef7c2085a71efde13ce9c8c69b32661c4e
adamPetho <petho.adam@hotmail.com>
2026-01-27 18:37:30 +0100

    clean up

--- LookUp.Core/Controllers/SearchController.cs
+++ LookUp.Core/Controllers/SearchController.cs
@@ -1,23 +1,19 @@
-﻿using LookUp.Models;
-using LookUp.Scanner.Cache;
+﻿using LookUp.Scanner.Cache;
 using LookUp.Scanner.DataBase;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 using NBitcoin.Protocol;
 
 namespace LookUp.Scanner.Controllers
 {
     public class SearchController : Controller
     {
-        public SearchController(MessageRepository messageRepository, IMemoryCache cache, IdempotencyCache idempotencyCache)
+        public SearchController(MessageRepository messageRepository, IdempotencyCache idempotencyCache)
         {
             MessageRepository = messageRepository;
-            Cache = cache;
             IdempotencyCache = idempotencyCache;
         }
 
         private MessageRepository MessageRepository { get; }
-        private IMemoryCache Cache { get; }
         private IdempotencyCache IdempotencyCache { get; }
 
         [HttpGet("/search")]
